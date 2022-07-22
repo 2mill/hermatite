5593c0ae0fa73bf876aa55039356bdb46a15cfcc
Yannick <brokeboat@protonmail.com>
2022-07-22 13:18:12 -0400

    adapter has been developed. I think it will need some adaption so that the responses can be placed into a deseralizer. I am having issues with the borrow checker while trying to accomplish this.

--- src/adapter.rs
+++ src/adapter.rs
@@ -17,14 +17,22 @@ impl ItemList {
 	}
 }
 
+
+enum TimeseriesTime {
+	FIVE_MIN,
+	ONE_HOUR,
+	SIX_HOURS
+}
+
+enum TimestampTime {
+	FIVE_MIN,
+	ONE_HOUR,
+}
 pub enum Endpoint {
 	Mapping,
 	Latest(Option<u32>),
 	Timeseries(TimeseriesTime, u32),
 	Timestamp(TimestampTime)
-}
-trait Time {
-	fn get_time() -> String;
 }
 
 
@@ -45,11 +53,14 @@ impl Config {
 }
 
 const BASE: String = String::from("https://prices.runescape.wiki/api/v1/osrs");
+
+
+
 trait Adapter {
 	fn get_mapping() -> reqwest::blocking::Response;
-	fn get_timeseries(crate::adapter::TimeseriesTime) -> reqwest::Response;
-	fn get_timestamp() -> reqwest::Response;
-	fn get_latest(id: Option<u32>) -> reqwest::Response;
+	fn get_timeseries(id: u32, interval: TimeseriesTime) -> reqwest::blocking::Response;
+	fn get_timestamp(timestamp: TimestampTime) -> reqwest::blocking::Response;
+	fn get_latest(id: Option<u32>) -> reqwest::blocking::Response;
 }
 
 
@@ -70,16 +81,35 @@ impl Adapter for Adaptee {
 		reqwest::blocking::get(format!("{}/mapping", BASE)).unwrap()
 	}
 
-	fn get_timeseries(id, interval: TimeseriesTime) -> reqwest::blocking::Response {
+	fn get_timeseries(id: u32, interval: TimeseriesTime) -> reqwest::blocking::Response {
 		let time = match interval {
 			TimeseriesTime::FIVE_MIN => "5m",
 			TimeseriesTime::ONE_HOUR => "1h",
 			TimeseriesTime::SIX_HOURS => "6h"
 		};
 
-		reqwest::blocking::get(format!("{}/timeserie?id="))
-
+		reqwest::blocking::get(format!("{}/timeseries?timestep={}&id={}", BASE, time, id)).unwrap()
 	}
+
+	fn get_timestamp(timestamp: TimestampTime) -> reqwest::blocking::Response {
+		let time = match timestamp {
+			TimestampTime::FIVE_MIN => "5m",
+			TimestampTime::ONE_HOUR => "1h",
+		};
+
+		reqwest::blocking::get(format!("{}/{}", BASE, time)).unwrap()
+	}
+
+	fn get_latest(id: Option<u32>) -> reqwest::blocking::Response {
+		let endpoint = match id {
+			Some(id) => format!("/latest?id={}", id.to_string()),
+			None => String::from("/latest")
+		};
+
+		reqwest::blocking::get(format!("{}{}", BASE, endpoint)).unwrap()
+	}
+
+
 
 
 }
